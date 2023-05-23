4742e89e5766dda1e64b6c1f19cd4fd29192a02a
alexander321321 <treesome@yandex.ru>
2023-05-23 21:53:21 +0200

    6. user.model

--- config/default.ts
+++ config/default.ts
@@ -1,4 +1,5 @@
 export default {
   port: 1337,
-  dbUri: "mongodb+srv://Alexander:12345@tomtech.7yp1a1d.mongodb.net/"
+  dbUri: "mongodb+srv://Alexander:12345@tomtech.7yp1a1d.mongodb.net/",
+  saltWorkFactor: 10
 }
