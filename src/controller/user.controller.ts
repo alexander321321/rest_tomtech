88aac8ca3d009f1b8361901cdd6c80cd081e1f93
alexander321321 <treesome@yandex.ru>
2023-05-23 23:06:20 +0200

    8. user.schema

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -5,6 +5,7 @@ import { createUser } from '../service/user.service';
 export async function createUserHandler(req: Request, res: Response) {
   try {
     const user = await createUser(req.body);
+    return user;
   } catch (e) {
     logger.error(e);
     return res.status(409).send(e.message)
