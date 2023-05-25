171aacb5ed22e5372c0b88cb54459ffee78ede49
alexander321321 <treesome@yandex.ru>
2023-05-25 22:11:42 +0200

    13. work with session.controller

--- src/controller/session.controller.ts
+++ src/controller/session.controller.ts
@@ -1,8 +1,19 @@
 import { Request, Response } from "express";
+import { validatePassword } from "../service/user.service";
+import { createSession } from "../service/session.service";
 
 export async function createUserSessionHandler(req: Request, res: Response) {
+
   // 1. Validate the user's password
+  const user = await validatePassword(req.body);
+
+  if (!user) {
+    return res.status(401).send("Invalid email or password");
+  }
+
   // 2. Create a session
+  const session = createSession(user._id, req.get("user-agent") || "")
+
   // 3. Create an access token
   // 4. Create a refresh token
   // 5. Return access & refresh tokens
